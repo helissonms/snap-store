9db894ab4e7d58fb2717e4b1e5b67f72bd3390e0
helissonms <helisson.ms@gmail.com>
2019-12-15 09:55:41 -0300

    [add] Enable hot reload.

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const renderRoot = (Component) => {
+  ReactDOM.render(<Component />, document.getElementById('root'));
+};
+
+renderRoot(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextRoot = require('./App').default;
+    renderRoot(NextRoot);
+  });
+}
